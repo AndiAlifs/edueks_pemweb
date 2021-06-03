9c867ef0ba06dc539e8b03a2e01a211b0734a4d4
Alfarrel Indrawan <alfarrelindrawan@gmail.com>
2021-06-03 14:24:08 +0700

    Membuat halaman jurusan dan menyambungkan navbar antar halaman

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,9 +2,15 @@
 
 class Home extends CI_Controller {
     public function index() {
+        $this->load->helper('url');
         $this->load->view('home');
     }
     public function univ() {
+        $this->load->helper('url');
         $this->load->view('review_univ');
+    }
+    public function jurusan() {
+        $this->load->helper('url');
+        $this->load->view('review_jurusan');
     }
 }
--- application/views/review_univ.php
+++ application/views/review_univ.php
@@ -10,20 +10,20 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">EduEx</a>
+    <a class="navbar-brand" href="<?php echo base_url('index.php/home')?>">EduEx</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+        <a class="nav-link" aria-current="page" href="<?php echo base_url('index.php/home')?>">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">Universitas</a>
+        <a class="nav-link active" href="<?php echo base_url('index.php/home/univ')?>">Universitas</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Jurusan</a>
+          <a class="nav-link" href="<?php echo base_url('index.php/home/jurusan')?>">Jurusan</a>
         </li>
       </ul>
       <a href="#" class="nav-link">Login</a>
