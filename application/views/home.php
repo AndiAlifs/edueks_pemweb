6a822a9cb47b825c517bda26a896ec2f69c5420a
Alfarrel Indrawan <alfarrelindrawan@gmail.com>
2021-06-13 14:35:43 +0700

    logo

--- application/views/home.php
+++ application/views/home.php
@@ -12,7 +12,7 @@
   <div class="container-fluid">
     
     <a class="navbar-brand" href="<?php echo base_url('index.php/home/index/' . $cek)?>">
-      <img src="<?php echo $this->config->item('base_url'); ?>logo_tanpa_slogan.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      <img src="<?php echo $this->config->item('base_url'); ?>logo_tanpa_slogan.png" alt="" width="100" height="100" class="d-inline-block align-text-middle">
       Eduex
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,7 +41,7 @@
     </div>
   </div>
 </nav>
-<img src="<?php echo $this->config->item('base_url'); ?>logo.png" class="img-fluid" alt="Logo Eduex">
+<img src="<?php echo $this->config->item('base_url'); ?>logo.png" class="img-fluid" alt="Logo Eduex" width="50" height="50">
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </html>
