8f0507034e05aea7d3a6e45b4ffbdd5ceb0cf33a
Alfarrel Indrawan <alfarrelindrawan@gmail.com>
2021-06-13 20:47:22 +0700

    Profil dan review

--- application/views/profil.php
+++ application/views/profil.php
@@ -7,10 +7,13 @@
     <title>Profil Pengguna</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body style="background-color: #C3F0E5;">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2fdf59;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo base_url('index.php/home/index/' . $cek)?>">EduEx</a>
+      <a class="navbar-brand" href="<?php echo base_url('home/index/' . $cek) ?>">
+        <img src="<?php echo $this->config->item('base_url'); ?>images/logo1.png" alt="" width="30" height="24" class="d-inline-block align-text-middle">
+        EduEx
+      </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -26,58 +29,72 @@
         <a class="nav-link" href="<?php echo base_url('index.php/home/jurusan/' . $cek)?>">Jurusan</a>
         </li>
       </ul>
+      <ul class="navbar-nav">
       <?php
         if($cek == 1) { ?>
+          <li class="nav-item">
           <a class="nav-link active" href="<?php echo base_url('index.php/home/profil/1')?>" class="nav-link">Profil</a>
+          </li>
+          <li class="nav-item">
           <a class="nav-link" href="<?php echo base_url('index.php/home/index/0')?>" class="nav-link">Log Out</a>
+          </li>
         <?php } else { ?>
+          <li class="nav-item">
           <a class="nav-link" href="<?php echo base_url('index.php/home/login')?>" class="nav-link">Login</a>
+          </li>
         <?php }
       ?>
+      </ul>
     </div>
   </div>
 </nav>
-<h1>Profil Pengguna</h1>
-<h5>Nama: <?= $profil->pnama?></h5>
-<h5>Email: <?= $profil->email?></h5>
-<h5>Universitas: <?= $profil->unama?></h5>
-<h5>Jurusan: <?= $profil->jnama?></h5>
-<h5>Angkatan: <?= $profil->angkatan?></h5>
-<h5>Keminatan: <?= $profil->keminatan?></h5>
-<br>
-<h4>Ulasan:</h4>
-<h5>Universitas</h5>
-<?php if(!isset($univ[0])) { ?>
-  <form action="<?= base_url('index.php/home/review/1')?>" method="post">
-    <input type="hidden" name="review" value="univ">
-    <button type="submit">Membuat Review</button>
-  </form>
-<?php } else { ?>
-  <form action="<?= base_url('index.php/home/review_update/1')?>" method="post">
-    <input type="hidden" name="review" value="univ">
-    <button type="submit">Mengubah Review</button>
-  </form>
-  <form action="<?= base_url('index.php/home/review_delete/')?>" method="post">
-    <input type="hidden" name="review" value="univ">
-    <button type="submit">Menghapus Review</button>
-  </form>
-<?php } ?>
-<h5>Jurusan</h5>
-<?php if(!isset($jurusan[0])) { ?>
-  <form action="<?= base_url('index.php/home/review/1')?>" method="post">
+<div class="container p-4" style="background-color: #DCFFFF;">
+  <h1 style="margin-bottom: 0.7em;">Profil Pengguna</h1>
+  <h5 class="my-3">Nama: <?= $profil->pnama?></h5>
+   <h5 class="my-3">Email: <?= $profil->email?></h5>
+    <h5 class="my-3">Universitas: <?= $profil->unama?></h5>
+    <h5 class="my-3">Jurusan: <?= $profil->jnama?></h5>
+    <h5 class="my-3">Angkatan: <?= $profil->angkatan?></h5>
+    <h5 class="my-3">Keminatan: <?= $profil->keminatan?></h5>
+    <br>
+    <h4>Ulasan:</h4>
+    <h5 class="my-3">Universitas</h5>
+    <?php if(!isset($univ[0])) { ?>
+      <form action="<?= base_url('index.php/home/review/1')?>" method="post">
+      <input type="hidden" name="review" value="univ">
+      <button class="btn btn-success" type="submit">Membuat Review</button>
+    </form>
+    <?php } else { ?>
+    <div class="d-grid gap-2 d-md-block">
+      <form action="<?= base_url('index.php/home/review_update/1')?>" method="post">
+      <input type="hidden" name="review" value="univ">
+      <button class="btn btn-success" type="submit">Mengubah Review</button>
+      </form>
+      <form action="<?= base_url('index.php/home/review_delete/')?>" method="post">
+      <input type="hidden" name="review" value="univ">
+      <button class="btn btn-success" type="submit">Menghapus Review</button>
+    </form>
+    </div>
+  <?php } ?>
+  <h5 class="my-3">Jurusan</h5>
+  <?php if(!isset($jurusan[0])) { ?>
+    <form action="<?= base_url('index.php/home/review/1')?>" method="post">
     <input type="hidden" name="review" value="jurusan">
-    <button type="submit">Membuat Review</button>
+    <button class="btn btn-success" type="submit">Membuat Review</button>
   </form>
-<?php } else { ?>
-  <form action="<?= base_url('index.php/home/review_update/1')?>" method="post">
+  <?php } else { ?>
+  <div class="d-grid gap-2 d-md-block">
+    <form action="<?= base_url('index.php/home/review_update/1')?>" method="post">
     <input type="hidden" name="review" value="jurusan">
-    <button type="submit">Mengubah Review</button>
-  </form>
-  <form action="<?= base_url('index.php/home/review_delete/')?>" method="post">
+    <button class="btn btn-success" type="submit">Mengubah Review</button>
+    </form>
+    <form action="<?= base_url('index.php/home/review_delete/')?>" method="post">
     <input type="hidden" name="review" value="jurusan">
-    <button type="submit">Menghapus Review</button>
-  </form>
-<?php } ?>
+    <button class="btn btn-success" type="submit">Menghapus Review</button>
+    </form>
+  </div>
+  <?php } ?>
+</div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </html>
--- application/views/review.php
+++ application/views/review.php
@@ -7,53 +7,68 @@
     <title>Profil Pengguna</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="<?php echo base_url('index.php/home/index/' . $cek)?>">EduEx</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<body style="background-color: #C3F0E5;">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2fdf59;">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="<?php echo base_url('home/index/' . $cek) ?>">
+        <img src="<?php echo $this->config->item('base_url'); ?>images/logo1.png" alt="" width="30" height="24" class="d-inline-block align-text-middle">
+        EduEx
+      </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="<?php echo base_url('index.php/home/index/' . $cek)?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo base_url('index.php/home/univ/' . $cek)?>">Universitas</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url('index.php/home/jurusan/' . $cek)?>">Jurusan</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+      <?php
+        if($cek == 1) { ?>
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="<?php echo base_url('index.php/home/index/' . $cek)?>">Home</a>
+          <a class="nav-link active" href="<?php echo base_url('index.php/home/profil/1')?>" class="nav-link">Profil</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url('index.php/home/univ/' . $cek)?>">Universitas</a>
+          <a class="nav-link" href="<?php echo base_url('index.php/home/index/0')?>" class="nav-link">Log Out</a>
           </li>
+        <?php } else { ?>
           <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url('index.php/home/jurusan/' . $cek)?>">Jurusan</a>
+          <a class="nav-link" href="<?php echo base_url('index.php/home/login')?>" class="nav-link">Login</a>
           </li>
-        </ul>
-        <?php
-          if($cek == 1) { ?>
-            <a class="nav-link active" href="<?php echo base_url('index.php/home/profil/1')?>" class="nav-link">Profil</a>
-            <a class="nav-link" href="<?php echo base_url('index.php/home/index/0')?>" class="nav-link">Log Out</a>
-          <?php } else { ?>
-            <a class="nav-link" href="<?php echo base_url('index.php/home/login')?>" class="nav-link">Login</a>
-          <?php }
-        ?>
-      </div>
+        <?php }
+      ?>
+      </ul>
     </div>
-  </nav>
-  <a href="<?php echo base_url('index.php/home/profil/1')?>" class="nav-link">Kembali</a>
-  <?php if(isset($univ) || isset($jurusan)) {
-    ?> <h3>Mengubah Review:</h3>
-  <?php } else { ?>
-  <h3>Membuat Review:</h3>
-  <?php } ?>
-  <form action="<?php echo base_url("index.php/home/review_process")?>" method="post">
+  </div>
+</nav>
+  <div class="container p-3" style="background-color: #DCFFFF;">
+    <a href="<?php echo base_url('index.php/home/profil/1')?>" class="">
+      <img src="<?php echo $this->config->item('base_url'); ?>images/back.svg" alt="" width="35" height="35">
+    </a>
     <?php if(isset($univ) || isset($jurusan)) {
-      ?> <input type="hidden" name="bentuk" value="ubah">
+      ?> <h3 class="m-3">Mengubah Review:</h3>
     <?php } else { ?>
-      <input type="hidden" name="bentuk" value="tambah">
+    <h3 class="m-3">Membuat Review:</h3>
     <?php } ?>
-    <input type="hidden" name="jenis" value="<?= $_POST['review']; ?>">
-    <textarea name="review" id="" cols="100" rows="15"><?php if(isset($univ) && $_POST['review'] == "univ") echo $univ[0]->review; elseif(isset($jurusan) && $_POST['review'] == "jurusan") echo $jurusan[0]->review; else ""?></textarea>
-    <br>
-    <button type="submit">KIRIM</button>
-  </form>
+    <form action="<?php echo base_url("index.php/home/review_process")?>" method="post">
+      <?php if(isset($univ) || isset($jurusan)) {
+        ?> <input type="hidden" name="bentuk" value="ubah">
+      <?php } else { ?>
+        <input type="hidden" name="bentuk" value="tambah">
+      <?php } ?>
+      <input type="hidden" name="jenis" value="<?= $_POST['review']; ?>">
+      <textarea placeholder="Silahkan isi review kamu sesuai dari pengalaman kamu selengkap-lengkapnya" class="m-3" name="review" id="" cols="100" rows="15"><?php if(isset($univ) && $_POST['review'] == "univ") echo $univ[0]->review; elseif(isset($jurusan) && $_POST['review'] == "jurusan") echo $jurusan[0]->review; else ""?></textarea>
+      <br>
+      <button type="submit" class="btn btn-success m-3 btn-lg">KIRIM</button>
+    </form>
+  </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </html>
